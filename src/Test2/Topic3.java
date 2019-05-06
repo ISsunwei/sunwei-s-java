c5665a48158a0d7d54fe56061ee986ef32880190
ISsunwei <sunwei2505906103@163.com>
2019-05-06 22:05:30 +0800

    常用类第三题

--- src/Test2/Topic3.java
+++ src/Test2/Topic3.java
@@ -6,5 +6,8 @@ public class Topic3 {
 		int num = 1;
 		Integer m = new Integer(num);
 		int m2 = m.intValue();           //基本数据类型和包装类的互相转化
+		
+		String n = m.toString();
+		Integer n2 = new Integer(n);     //包装类和字符串相互转换
 	}
 }
