f96f4add231fc6fbed27721c9cbe9e70c6a34635
ISsunwei <sunwei2505906103@163.com>
2019-05-06 22:01:59 +0800

    常用类第三题

---
+++ src/Test2/Topic3.java
@@ -0,0 +1,10 @@
+package Test2;
+
+public class Topic3 {
+
+	public static void main(String[] args) {
+		int num = 1;
+		Integer m = new Integer(num);
+		int m2 = m.intValue();           //基本数据类型和包装类的互相转化
+	}
+}
