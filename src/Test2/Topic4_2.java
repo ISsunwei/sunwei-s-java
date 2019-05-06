e7a1ad84577040d57817c0c614ca13b192409eba
ISsunwei <sunwei2505906103@163.com>
2019-05-06 23:35:11 +0800

    常用类第四题第二小题

---
+++ src/Test2/Topic4_2.java
@@ -0,0 +1,13 @@
+package Test2;
+
+import java.util.Scanner;
+
+//将一个字符串中所有的ip地址打印出来
+public class Topic4_2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入带IP地址的字符串：");
+		String str = input.nextLine();
+	}
+}
