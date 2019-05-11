d24a389300bd6ec87ca49b9421c0af9bd68479d2
ISsunwei <sunwei2505906103@163.com>
2019-05-11 14:13:44 +0800

    常用类第四题第二小题

--- src/Test2/Topic4_2.java
+++ src/Test2/Topic4_2.java
@@ -7,7 +7,12 @@ public class Topic4_2 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("ÇëÊäÈë´øIPµØÖ·µÄ×Ö·û´®£º");
+		System.out.println("ÇëÊäÈëIPµØÖ·×Ö·û´®£º");
 		String str = input.nextLine();
+		if(str.matches("\\d{1,3}\\.d{3}\\.d{1,3}")) {
+			System.out.println("str");
+		}
+		else
+			System.out.println("´íÎóIPµØÖ·");
 	}
 }
