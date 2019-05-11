6cc83f9978701f0fa782ef2038a64e24824d0537
ISsunwei <sunwei2505906103@163.com>
2019-05-11 16:58:17 +0800

    常用类第五题

--- src/Test2/Topic5.java
+++ src/Test2/Topic5.java
@@ -10,18 +10,31 @@ import java.util.Scanner;
 public class Topic5 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入一个日期（格式：yyyy-MM-dd（年月日））：");
+		String date = input.next();
+		int y = Integer.valueOf(date.substring(0, 4));
+        int m = Integer.valueOf(date.substring(5, 7));
+        int d = Integer.valueOf(date.substring(8));
+	    LocalDate localDate = LocalDate.of(y, m, d);
+	    System.out.println(localDate.plusDays(7).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+		
+		
+		
+		
+		
 		
 		LocalDate ld = LocalDate.now();
-		 
+		
 		DateTimeFormatter fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
 		System.out.println("Short Date: " + fmt.format(ld));
-		 
+		
 		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
 		System.out.println("Medium Date: " + fmt.format(ld));
-		 
+		
 		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
 		System.out.println("Long Date: " + fmt.format(ld));
-		 
+		
 		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
 		System.out.println("Full Date: " + fmt.format(ld));
 		}
