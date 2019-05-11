a78475bc4c6f52047e3ad5ceba457a890488fac6
ISsunwei <sunwei2505906103@163.com>
2019-05-11 15:51:18 +0800

    常用类第四题第九小题

---
+++ src/Test2/Topic4_9.java
@@ -0,0 +1,10 @@
+package Test2;
+
+//文本替换：将字符串中所有的”pupel”替换为”pupil”
+public class Topic4_9 {
+
+	public static void main(String[] args) {
+		String str = "pupel";
+        System.out.println(str.replaceAll("pupel","pupil"));
+	}
+}
