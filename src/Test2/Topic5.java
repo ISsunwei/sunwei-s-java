c3c722ad2cbdd5e56120ec1995b1825ea2135535
ISsunwei <sunwei2505906103@163.com>
2019-05-11 16:48:07 +0800

    常用类第五题

--- src/Test2/Topic5.java
+++ src/Test2/Topic5.java
@@ -1,23 +1,28 @@
 package Test2;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.time.temporal.TemporalAccessor;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Scanner;
 
 
 public class Topic5 {
 
 	public static void main(String[] args) {
-		Date date = Topic5.dateChange(+7);
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
-		System.out.println(dateFormatter.toFormat());
-	}
-	
-	public static Date dateChange(int days) {
-		Calendar canlendar = Calendar.getInstance();
-		canlendar.add(Calendar.DATE, days);
-		return canlendar.getTime();
-	}
+		
+		LocalDate ld = LocalDate.now();
+		 
+		DateTimeFormatter fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+		System.out.println("Short Date: " + fmt.format(ld));
+		 
+		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+		System.out.println("Medium Date: " + fmt.format(ld));
+		 
+		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+		System.out.println("Long Date: " + fmt.format(ld));
+		 
+		fmt = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+		System.out.println("Full Date: " + fmt.format(ld));
+		}
 }
