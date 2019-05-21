dceb37a7e283b5a225a5ac172a10d18649691fb4
ISsunwei <sunwei2505906103@163.com>
2019-05-21 21:13:07 +0800

    工具类修改

--- src/Test3/Tool.java
+++ src/Test3/Tool.java
@@ -2,8 +2,14 @@ package Test3;
 
 public class Tool {
 
-	public static int parseInt(String str) throws MyExceptionTest {
-		return Integer.parseInt(str);
-	
+	public static  int parseInt(String str) throws MyExceptionTest{
+			for(int i=0;i<str.length();i++) {
+				char ch = str.charAt(i);
+				if(ch>='0'&&ch<='9') {
+					System.out.println(ch);
+				}else
+					throw new MyExceptionTest("´íÎóÒì³£");
+			}
+			return ch;
 	}
 }
