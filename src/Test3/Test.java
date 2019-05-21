6b4cdb0857654682f064d07c7a553d09a0f82a90
ISsunwei <sunwei2505906103@163.com>
2019-05-21 20:21:42 +0800

    测试

--- src/Test3/Test.java
+++ src/Test3/Test.java
@@ -1,12 +1,17 @@
 package Test3;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("ÇëÊäÈëÒ»´®×Ö·û´®");
+		String s = input.nextLine();
 		try{
-            
-        }catch (NumberFormatException e){
-            
+            System.out.println(Tool.parseInt(s));
+        }catch (MyExceptionTest str){
+            System.out.println();
         }
 	}
 }
