a3007affea25284b73dc4272cac01d2949332d3b
ISsunwei <sunwei2505906103@163.com>
2019-05-22 19:39:01 +0800

    将体重的数据类型改成float

--- src/Test4/Apple.java
+++ src/Test4/Apple.java
@@ -1,5 +1,22 @@
 package Test4;
 
+/*
+ * 定义一个苹果类，有颜色和重量
+ */
 public class Apple {
 
+	private String color;
+	private float weight;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
 }
