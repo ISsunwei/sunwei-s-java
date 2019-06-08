5e566374d20b43de04cebdc2df3deaca7259e91e
ISsunwei <sunwei2505906103@163.com>
2019-06-08 14:49:59 +0800

    keyset方法

--- src/Test5/StudentMap.java
+++ src/Test5/StudentMap.java
@@ -1,53 +1,62 @@
 package Test5;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class StudentMap {
 
 	public static void main(String[] args) {
-		
-		HashMap<Student2,String> hm=new HashMap<Student2,String>();
-		hm.put(new Student2("张三",20),"福建");
-		hm.put(new Student2("李四",22),"浙江");
-		hm.put(new Student2("王五",22),"福建");
-		hm.put(new Student2("赵六",22),"广西");
+
+		HashMap<Student2, String> hm = new HashMap<Student2, String>();
+		hm.put(new Student2("张三", 20), "福建");
+		hm.put(new Student2("李四", 22), "浙江");
+		hm.put(new Student2("王五", 22), "福建");
+		hm.put(new Student2("赵六", 22), "广西");
+
+		// keyset
+		Set<Student2> ks = hm.keySet();
+		Iterator<Student2> it=ks.iterator();
+		while(it.hasNext()) {
+			Student2 st = it.next();
+			String addr=hm.get(st);
+			System.out.println(st+"---"+addr);
+		}
 	}
 }
 
-class Student2{
-	
+class Student2 {
+
 	private String name;
 	private int age;
-	public Student2(){}
-	public Student2(String name,int age) {
-		
+
+	public Student2() {
 	}
+
+	public Student2(String name, int age) {
+
+	}
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + age;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		int result;
+        result = (name==null?0:name.hashCode());
+        return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student2 other = (Student2) obj;
-		if (age != other.age)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+		Student2 students = (Student2) obj;
+
+        if(this.name.equals(students.name)&&this.age==students.age){
+            return true;
+        }
+        else return false;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Student2 [name=" + name + ", age=" + age + "]";
+	}
 	
 }
