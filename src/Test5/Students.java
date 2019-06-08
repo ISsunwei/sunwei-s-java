5c04df7d450f1e6ccd72f89dc53b4b2831e8995f
ISsunwei <sunwei2505906103@163.com>
2019-06-08 17:17:38 +0800

    重写方法

---
+++ src/Test5/Students.java
@@ -0,0 +1,16 @@
+package Test5;
+
+public class Students {
+
+	private int id;
+    private String name;
+    private double score;
+
+    public Students() {}
+    public Students(int id, String name, double score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+    
+}
