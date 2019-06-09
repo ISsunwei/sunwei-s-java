dd98e4a6463216e908db3b446c08e92dd289bb97
ISsunwei <sunwei2505906103@163.com>
2019-06-09 14:27:20 +0800

    按总分升序

--- src/Test5/Test2.java
+++ src/Test5/Test2.java
@@ -1,15 +1,23 @@
 package Test5;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		List<Student> list = new ArrayList<>();
-		list.add(new Student(1, "张三", 88));
-        list.add(new Student(2, "李四", 82));
-        list.add(new Student(3, "王五", 92));
-        list.add(new Student(4, "赵六", 79));
+		List<Students> list = new ArrayList<>();
+		list.add(new Students(1, "张三", 88));
+        list.add(new Students(2, "李四", 82));
+        list.add(new Students(3, "王五", 92));
+        list.add(new Students(4, "赵六", 79));
+        // 按总分升序
+        Collections.sort(list);
+        Iterator<Students> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 	}
 }
