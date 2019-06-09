9b3b6ccca87c12bd8fc799f2b146af4a7a6e97a7
ISsunwei <sunwei2505906103@163.com>
2019-06-09 15:16:07 +0800

    HashSet是如何检测是否有重复元素的，并写程序验证

--- src/Test5/Test3.java
+++ src/Test5/Test3.java
@@ -1,5 +1,8 @@
 package Test5;
 
+/**
+ * HashSet是如何检测是否有重复元素的，并写程序验证
+ */
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +19,21 @@ public class Test3 {
 		String[] names = set.toArray(new String[] {});
 		for(String s:names) {
 			System.out.println(s);
+		}
+		
+		Cat c1 = new Cat("白猫",5,1);
+		Cat c2 = new Cat("黑猫",2,2);
+		Cat c3 = new Cat("橘猫",4,3);
+		Cat c4 = new Cat("白猫",5,1);
+		Set<Cat> cats = new HashSet<Cat>();
+		cats.add(c1);
+		cats.add(c2);
+		cats.add(c3);
+		cats.add(c4);
+		System.out.println(cats.size());
+		
+		for(Cat c:cats) {
+			System.out.println(c);
 		}
 	}
 }
